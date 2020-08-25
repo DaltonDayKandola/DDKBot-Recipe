99bac44c0e1f8414cba8f17f5d947e5d278d7206
DDKLimited <william.dalton@ddklimited.com>
2020-08-25 12:35:41 +0100

    Add files via upload

---
+++ Dialogs/UserProfileDialog.cs
@@ -0,0 +1,48 @@
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotBuilderSamples
+{
+
+public class UserProfileDialog : ComponentDialog
+    {
+        private readonly IStatePropertyAccessor<UserProfile> _userProfileAccessor;
+
+    public UserProfileDialog(UserState userState)
+        : base(nameof(UserProfileDialog))
+        {
+            _userProfileAccessor = userState.CreateProperty<UserProfile>("UserProfile");
+
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {   
+                NameStepAsync,
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private static async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Text("Please enter your name.") }, cancellationToken);
+        }
+    }    
+
+}
+  
