ad1f150d11d4ad92eb9347f0fcec4f9f4486ba6f
william dalton <william.dalton@ddklimited.com>
2021-02-06 13:35:47 +0000

    Initial baseline

---
+++ Code/Application/UserProfile.cs
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+
+{
+    // Defines a state property used to track information about the user.
+
+    public class UserProfile
+
+    {
+
+        public string Name { get; set; }
+        public string ContactType { get; set; }
+        public string ContactValue { get; set; }
+        public bool ContactDialogueFlow { get; set; }
+
+    }
+
+}
